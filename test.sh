d00128f548b67246715b73d6fb2c506e73641666
Ted Lehr <t_l81@txstate.edu>
2017-04-05 17:37:45 -0500

    Add echoes to the test script

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
-echo "hey dude"
+echo "testing hwInput_GP"
 python helloworld.py < hwInput_GP.txt
+echo "testing hwInput_GP_quoted"
 python helloworld.py < hwInput_GP_quoted.txt
+echo "testing hwInput_2"
 python helloworld.py < hwInput_2.txt
+echo "testing hwInput_2_quoted"
 python helloworld.py < hwInput_2_quoted.txt
