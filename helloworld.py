7ef0132ff86f9d9716a1b20a9f0bbac39b34a9bc
Ted Lehr <t_l81@txstate.edu>
2017-10-18 17:34:15 -0500

    Moved the files to the dir that Travis Ci sees

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,1 @@
-try:
-	mode=int(input("Give me a number! "))
-	print("That number was: "),  mode, "\n"
-except ValueError:
-	print("That was string without a numeral\n")
-except NameError:
-	print("That was neither a numeral or a string\n")
-#except SyntaxError:
-	#print("Syntax Error:  Possibly an incorrectly formatted string\n")
+print("Hello world Fall 2017 CS3398! from Travis CI")
